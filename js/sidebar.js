27053bb162dacb1e5438547fff6aa7a7eb7ecb70
sethulaw <152017662+sethulaw@users.noreply.github.com>
2023-11-26 00:49:03 +0530

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,9 +2,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
