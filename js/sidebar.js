d6fa7e8ec73a6145e50032ce4815de9aac97171e
sethulaw <152017662+sethulaw@users.noreply.github.com>
2023-11-26 01:04:26 +0530

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,3 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
       var elem = document.querySelector('.sidenav');
       var instance = new M.Sidenav(elem);
   });
+
+$( document ).ready(function(){
+$('.button-collapse').sideNav({
+      closeOnClick: true
+    }
+  );
+});
